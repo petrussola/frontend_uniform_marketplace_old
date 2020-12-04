71cd9dff80972d051b9a3da5b816dd3038d64387
Pere <pere.solaclaver@gmail.com>
2020-12-04 23:01:00 +0000

    Place auth provider top of the app and install react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+
+// context
+import { AuthProvider } from "./context/AuthContext";
+
+// components
+import MarketingPage from "./components/MarketingPage";
 
 function App() {
   return (
     <div className="App">
-      <p>hello from dev version 2</p>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={MarketingPage} />
+        </Switch>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/components/MarketingPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MarketingPage() {
+  return <div>{`I am the marketing page in ${process.env.NODE_ENV}`}</div>;
+}
