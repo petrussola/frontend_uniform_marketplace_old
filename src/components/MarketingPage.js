ddf8e140f919b488489e225ac988bf38d1f6c2ce
Pere <pere.solaclaver@gmail.com>
2020-12-05 00:23:14 +0000

    Creat navbar with links to signup and login

--- src/components/MarketingPage.js
+++ src/components/MarketingPage.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export default function MarketingPage() {
   return (
-    <div>{`I am the marketing page in ${process.env.NODE_ENV} environment`}</div>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>Uniforms inc.</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="ml-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/signup">Sign Up</Nav.Link>
+            <Nav.Link href="/signin">Sign In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      {`I am the marketing page in ${process.env.NODE_ENV} environment`}
+    </div>
   );
 }
