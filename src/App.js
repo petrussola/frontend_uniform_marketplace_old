f793187923c8f54ac08b66c9eaa116cb5a3d4a3e
Pere <pere.solaclaver@gmail.com>
2020-12-12 23:37:17 +0000

    Delete test form component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { AuthProvider } from "./context/AuthContext";
 // components
 import MarketingPage from "./components/MarketingPage";
 import SignUp from "./components/Signup";
-import Form from "./components/Form";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={MarketingPage} />
           <Route path="/signup" component={SignUp} />
-          <Route path="/test" component={Form} />
         </Switch>
       </AuthProvider>
     </div>
