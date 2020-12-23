cbae4c83a6560155db08c554d68d92acbe5fc533
Pere <pere.solaclaver@gmail.com>
2020-12-23 22:09:59 +0000

    MOve routing and context provider login in tests to test-utils file

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,18 +1,10 @@
 import React from "react";
-import { render, screen, act } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Login from "./Login";
-// import { render, screen, act } from "../config/test-utils";
-import { AuthProvider } from "../context/AuthContext";
+import { render, screen, act } from "../config/test-utils";
 
 beforeEach(async () => {
-  const wrapper = ({ children }) => (
-    <Router>
-      <AuthProvider>{children}</AuthProvider>
-    </Router>
-  );
   await act(async () => {
-    render(<Login />, { wrapper });
+    render(<Login />);
   });
 });
 
