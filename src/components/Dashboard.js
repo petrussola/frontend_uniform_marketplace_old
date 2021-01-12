1b4841bdd956ea41041b83fceb45d68f5462f222
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-01-12 23:33:11 +0000

    Logout method in context

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function Dashboard() {
-  return <div>this is the dashboard</div>;
+  // const { logout } = useAuth();
+  const handleLogout = () => {
+    console.log("logout");
+  };
+  return (
+    <div>
+      <h1>This is the Dashboard</h1>
+      <button onlick={handleLogout} type="button">
+        Logout
+      </button>
+    </div>
+  );
 }
