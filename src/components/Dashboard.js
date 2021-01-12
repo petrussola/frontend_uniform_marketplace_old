69f09701a411744ca8a240a7c4c6337f495737c5
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-01-12 23:53:59 +0000

    Logout button in dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function Dashboard() {
-  // const { logout } = useAuth();
-  const handleLogout = () => {
-    console.log("logout");
+  const { currentUser, logout } = useAuth();
+  const [error, setError] = useState("");
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    // setError("");
+    try {
+      await logout();
+      history.push("/login");
+    } catch (e) {
+      setError(e.message);
+    }
   };
   return (
     <div>
+      {error && <p>{error}</p>}
       <h1>This is the Dashboard</h1>
-      <button onlick={handleLogout} type="button">
+      <h5>Email: {currentUser.email}</h5>
+      <button onClick={handleLogout} type="button">
         Logout
       </button>
     </div>
