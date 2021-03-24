61da3bb4596d56f49d0541e914cd7f43aa0a251e
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-03-24 23:14:38 +0000

    Responsive design marketing page

--- src/components/MarketingPage.js
+++ src/components/MarketingPage.js
@@ -12,6 +12,11 @@ const StyledDiv = styled.div`
   padding: 0 1em;
   background-color: #fcfcfc;
   height: 100vh;
+  position: relative;
+  box-sizing: border-box;
+  @media (max-width: 800px) {
+    height: auto;
+  }
 `;
 
 const StyledJumbotron = styled(Jumbotron)`
@@ -20,12 +25,18 @@ const StyledJumbotron = styled(Jumbotron)`
   flex-flow: row wrap;
   align-items: center;
   justify-content: space-evenly;
-  min-height: 100px;
+  height: auto;
+  @media (max-width: 800px) {
+    margin-bottom: 1rem;
+  }
   span {
     color: #15a545;
   }
   .parts-jumbotron {
     width: 50%;
+    @media (max-width: 800px) {
+      width: 100%;
+    }
   }
   .right-jumbotron {
     height: 300px;
@@ -36,15 +47,24 @@ const StyledJumbotron = styled(Jumbotron)`
     img {
       height: 100%;
       border-radius: 0.3rem;
+      @media (max-width: 800px) {
+        height: 75%;
+      }
     }
   }
   h1 {
     font-size: 2.5rem;
     padding: 0.2rem 0;
+    @media (max-width: 800px) {
+      font-size: 2rem;
+    }
   }
   p {
     padding: 0.2rem 0;
     font-size: 1.5rem;
+    @media (max-width: 800px) {
+      font-size: 1.25rem;
+    }
   }
 `;
 
@@ -59,11 +79,18 @@ const StyledButton = styled(Button)`
 `;
 
 const StyledFooter = styled.footer`
-  bottom: 1rem;
+  bottom: 0;
+  left: 0;
   position: absolute;
-  width: 90%;
+  width: 100%;
+  padding-top: 1rem;
   margin: 0 auto;
   text-align: center;
+  background-color: #e7e7e7;
+  color: #959595;
+  @media (max-width: 600px) {
+    margin-top: 1rem;
+  }
 `;
 
 export default function MarketingPage() {
@@ -71,16 +98,19 @@ export default function MarketingPage() {
     <StyledDiv>
       <Navbar expand="lg">
         <Navbar.Brand>Share Uniforms</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          {process.env.REACT_APP_IS_SITE_LIVE === "false" ? null : (
-            <Nav className="ml-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-              <Nav.Link href="/login">Sign In</Nav.Link>
-            </Nav>
-          )}
-        </Navbar.Collapse>
+
+        {process.env.REACT_APP_IS_SITE_LIVE === "false" ? null : (
+          <>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse>
+              <Nav className="ml-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+                <Nav.Link href="/login">Sign In</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        )}
       </Navbar>
       <StyledJumbotron>
         <div className="left-jumbotron parts-jumbotron">
@@ -89,8 +119,8 @@ export default function MarketingPage() {
             <span> Very fast.</span>
           </h1>
           <p>
-            Marketplace for a community of parents to share unused uniforms so
-            others can get the size they need for free or at a reduced price.
+            Marketplace for communities of parents to donate or sell school
+            uniforms so others can reuse them.
           </p>
           <StyledButton size="lg" block disabled>
             Coming soon to your local school
@@ -100,7 +130,12 @@ export default function MarketingPage() {
           <img src={uniformOne} alt="school uniform" />
         </div>
       </StyledJumbotron>
-      <StyledFooter>Made with love by peresola.com</StyledFooter>
+      <StyledFooter>
+        <p>
+          © 2021 ShareUniforms.com. All rights reserved. Made with 💙 by{" "}
+          <a href="https://www.peresola.com">peresola.com</a>
+        </p>
+      </StyledFooter>
     </StyledDiv>
   );
 }
